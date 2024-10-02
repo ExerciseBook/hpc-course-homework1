a96e2054907ae23e55f1c12a2e7e3246039a7109
Eric_Lian <git@superexercisebook.com>
2024-10-02 15:53:18 +0800

    feat: adjust packing

--- sgemm-blocked.cpp
+++ sgemm-blocked.cpp
@@ -13,27 +13,28 @@ const char* sgemm_desc = "Simple blocked sgemm.";
 
 void do_block(
         // clang-format off
-    int M, int K, int N, 
-    int strideA, int strideB, int strideC,
-    float *A, float *B, float *C,
+        int M, int K, int N,
+        int strideA, int strideB, int strideC,
+        float *A, float *B, float *C,
 
-    float* packed_B
+        float* packed_A
         // clang-format on
 ) {
-    for (int j = 0; j < N; ++j) {
-        for (int i = 0; i < M; ++i) {
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
             float cij = C(i, j);
             for (int k = 0; k < K; ++k) {
-                // cij += A(i, k) * B[k + strideB * j] ;
-                cij += A(i, k) * packed_B[j + k * BLOCK_N];
+                // 所以 A的 i行k列 被映射到了 packB的 k行i列
+                // cij += A[i + strideA* k]* B[k + strideB * j] ;
+                cij += packed_A[k + i * BLOCK_K] * B[k + strideB * j];
             }
             C(i, j) = cij;
         }
     }
 }
 
-// 原来B的 i行j列 被打包到了 j行i列
-void packB(float* dst, int row, int width, int stride, float* src) {
+// 原来A的 i行j列 被打包到了 j行i列
+void packA(float* dst, int row, int width, int stride, float* src) {
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < width; j++) {
             dst[width * i + j] = src[stride * j + i];
@@ -54,25 +55,22 @@ void custom_sgemm(int M, int K, int N, float* A, float* B, float* C) {
     //  M行 N列
     auto strideC = M;
 
-    float* packed_B;
-    if (posix_memalign((void**) &packed_B, 4096, BLOCK_N * BLOCK_K * sizeof(float)) != 0) {
+    float* packed_A;
+    if (posix_memalign((void**) &packed_A, 4096, BLOCK_M * BLOCK_K * sizeof(float)) != 0) {
         throw std::runtime_error("Can not align malloc packed pool!");
     }
 
-    // 遍历 B 的每一行
-    for (int n = 0; n < N; n += BLOCK_N) {
-        for (int k = 0; k < K; k += BLOCK_K) {
-            packB(packed_B, BLOCK_K, BLOCK_N, strideB, &B(k, n));
-            // print_matrix(&B(k, n), strideB, BLOCK_K, BLOCK_N);
-            // print_matrix(packed_B, BLOCK_K, BLOCK_N, BLOCK_K);
+    for (int k = 0; k < K; k += BLOCK_K) {
+        int K0 = min(BLOCK_K, K - k);
+        for (int m = 0; m < M; m += BLOCK_M) {
+            int M0 = min(BLOCK_M, M - m);
+            packA(packed_A, BLOCK_M, BLOCK_K, strideA, &A(m, k));
+            // print_matrix(&A(k, m), strideA, BLOCK_M, BLOCK_K);
+            // print_matrix(packed_A, BLOCK_K, BLOCK_K, BLOCK_M);
 
-            for (int m = 0; m < M; m += BLOCK_M) {
-                int M0 = min(BLOCK_M, M - m);
-                int K0 = min(BLOCK_K, K - k);
+            for (int n = 0; n < N; n += BLOCK_N) {
                 int N0 = min(BLOCK_N, N - n);
-
-                // 优先遍历 B 来减少转置 B 时的开销
-                do_block(M0, K0, N0, strideA, strideB, strideC, &(A(m, k)), &(B(k, n)), &(C(m, n)), packed_B);
+                do_block(M0, K0, N0, strideA, strideB, strideC, &(A(m, k)), &(B(k, n)), &(C(m, n)), packed_A);
             }
         }
     }
